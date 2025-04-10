997b7b4a92813e5b21017a0e9ed7e733cdb9dbeb
OmarOlate <omarolate.96@gmail.com>
2025-04-09 23:19:08 -0400

    feat(auth): add bearer token request

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,10 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { setupSwagger } from '@nest-js-monorepo-base-project/swagger';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from 'libs/auth/features/guards/jwt-token.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +18,10 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
+  const reflector = app.get(Reflector);
+  const jwtService = app.get(JwtService);
+  app.useGlobalGuards(new AuthGuard(jwtService, reflector));
 
   setupSwagger(app, appName);
   app.useGlobalPipes(new ValidationPipe());
